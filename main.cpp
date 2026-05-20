d372a6a8e5ad8a528146471684092b02be1b44d7
Rafaelgm04 <rafaelgmbr04@gmail.com>
2026-05-19 23:53:54 -0400

    Refactor DesConverte method and implement base-10 addition and multiplication functions

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ class BigNum
 
     }
 
-    void DesCoverte(){
+    void DesConverteSimples()const{
        unsigned long long num = 0;
         for(size_t i =0;i < Big.size();i++){
             num += Big[i] * pow(256,i);
@@ -48,7 +48,50 @@ class BigNum
         cout << num;
     }
 
+    string soma_base_10(string num_string1,string num_string2)const{
+        size_t tam = max(num_string1.size(),num_string2.size());
+        reverse(num_string1.begin(),num_string1.end());
+        reverse(num_string2.begin(),num_string2.end());
+        string resu;
+        uint8_t carry =0;
+        for(size_t i = 0;i < tam;i++){
 
+            uint8_t a = 0;
+            uint8_t b = 0;
+
+            if(i < num_string1.size())
+                a = num_string1.at(i) - '0';
+            if(i < num_string2.size())
+                b = num_string2.at(i) - '0';
+
+
+
+
+            uint8_t s = a + b + carry;
+
+            if(s > 9){
+                carry = s/10;
+            }else{
+                carry = 0;
+            }
+            resu.push_back(s%10 + '0');
+        }
+        if(carry)
+            resu.push_back(carry + '0');
+
+
+        reverse(resu.begin(),resu.end());
+        return resu;
+    }
+
+    string multiplica(string &num_string,unsigned num)const{
+        string resu = num_string;
+        for(size_t i =0;i < num;i++){
+            resu = soma_base_10(resu,num_string);
+        }
+
+        return resu;
+    }
 
 public:
 
@@ -139,6 +182,16 @@ public:
             cout << (int)x << " ";
         }
     }
+
+
+    string DesConverte()const{       
+        string num = "0";
+        for(size_t i=0;i < Big.size();i++){
+            num = multiplica(num,(unsigned)256);
+            num = soma_base_10(num,to_string(Big.at(i) ) );
+        }
+    }
+
 };
 
 istream &operator>>(istream &input, const string &Num) {
@@ -150,8 +203,55 @@ istream &operator>>(istream &input, const string &Num) {
 
 }
 
+/*
+    string soma_base_10(string num_string1,string num_string2){
+        size_t tam = max(num_string1.size(),num_string2.size());
+        reverse(num_string1.begin(),num_string1.end());
+        reverse(num_string2.begin(),num_string2.end());
+        string resu;
+        uint8_t carry =0;
+        for(size_t i = 0;i < tam;i++){
+
+            uint8_t a = 0;
+            uint8_t b = 0;
+
+            if(i < num_string1.size())
+                a = num_string1.at(i) - '0';
+            if(i < num_string2.size())
+                b = num_string2.at(i) - '0';
+
+
+
+
+            uint8_t s = a + b + carry;
+
+            if(s > 9){
+                carry = s/10;
+            }else{
+                carry = 0;
+            }
+            resu.push_back(s%10 + '0');
+        }
+        if(carry)
+            resu.push_back(carry + '0');
+
+
+        reverse(resu.begin(),resu.end());
+        return resu;
+    }
+
+    string multiplica(string &num_string,unsigned num){
+        string resu = num_string;
+        for(size_t i =0;i < num;i++){
+            resu = soma_base_10(resu,num_string);
+        }
+
+        return resu;
+    }
+
+*/
 int main()
-{   
+{   /*
     string num("123456789");
 
     BigNum teste("257");
@@ -164,6 +264,13 @@ int main()
     teste.mostra_bytes();
     cout << "\n";
     //teste.DesCoverte();
+    */
+    //string num = "10";
+    //cout << (soma_base_10(num,"1234321")) << "\n";
+
+    //cout << multiplica(num,2);
+
+
 
     return 0;
 }
